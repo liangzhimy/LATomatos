2bb078c51f0336dcbd756da6de3d8ad24c2c0226
liangzhimy <liangzhimy@gmail.com>
2017-06-13 13:51:11 +0800

    Initial Commit

---
+++ LATomatos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LATomatos
+//
+//  Created by liangzhimy on 17/6/13.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
